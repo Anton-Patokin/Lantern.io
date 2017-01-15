ea066988a7956369b659e3b04eedaefd8d9155f5
Anton-Patokin <patokin.anton@gmail.com>
2017-01-15 13:45:29 +0100

    add crone job, remove rooms older than 1 week

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        Commands\RemoveRoom::class,
     ];
 
     /**
@@ -26,6 +26,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        $schedule->command('room:remove')->dailyAt('00:00');
     }
 
     /**
